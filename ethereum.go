904ec68859e11b5aa2e12a3e80ec49a8a935adc3
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-05-17 01:10:09 +0000

    os.WriteFile

--- ethereum.go
+++ ethereum.go
@@ -1,56 +1,54 @@
 package main
 
 import (
-	"os"	
 	"crypto/ecdsa"
 	"fmt"
+	"os"
 	// "math/rand"
-	// "time"	
+	"time"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/onrik/ethrpc"
 )
 
-func Ethereum(done chan<- bool) {
+func Ethereum(done chan bool) {
 	rpc := "https://api.bitstack.com/v1/wNFxbiJyQsSeLrX8RRCHi7NpRxrlErZk/DjShIqLishPCTB9HiMkPHXjUM9CNM9Na/ETH/mainnet"
 
-	for {
-		privateKey, err := crypto.GenerateKey()
-		if err != nil {
-			panic(err)
-		}
-
-		publicKey := privateKey.Public()
-		publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-		if !ok {
-			panic("Could not cast public key to ECDSA")
-		}
-
-		address := crypto.PubkeyToAddress(*publicKeyECDSA)
-
-		rpcClient := ethrpc.New(rpc)
-		balance, err := rpcClient.EthGetBalance(address.String(), "latest")
-		if err != nil {
-			panic(err)
-		}
-
-		if balance.Int64() > 0 {
-			fmt.Printf("The address %s has a balance of %s ETH\n", address.Hex(), balance.String())
-
-			privateKeyBytes := crypto.FromECDSA(privateKey)
-			err = os.WriteFile("eth_private_key.txt", privateKeyBytes, 0644)
-			if err != nil {
-				panic(err)
-			}
-		} else {
-			fmt.Printf("The address %s has no balance\n", address.Hex())
-		}
-
-		//  rand.Seed(time.Now().UnixNano())
-		//	waitTime := rand.Intn(10) + 1
-		// 	fmt.Printf("Waiting %d seconds before generating the next address\n", waitTime)
-		// 	time.Sleep(time.Duration(waitTime) * time.Second)
-
-		done <- true
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		panic(err)
 	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		panic("Could not cast public key to ECDSA")
+	}
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	rpcClient := ethrpc.New(rpc)
+	balance, err := rpcClient.EthGetBalance(address.String(), "latest")
+	if err != nil {
+		panic(err)
+	}
+
+	if balance.Int64() > 0 {
+		fmt.Printf("The address %s has a balance of %s ETH\n", address.Hex(), balance.String())
+
+		privateKeyBytes := crypto.FromECDSA(privateKey)
+		err = os.WriteFile("eth_private_key.txt", privateKeyBytes, 0644)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		fmt.Printf("The address %s has no balance\n", address.Hex())
+	}
+
+	//  rand.Seed(time.Now().UnixNano())
+	//	waitTime := rand.Intn(10) + 1
+	// 	fmt.Printf("Waiting %d seconds before generating the next address\n", waitTime)
+	time.Sleep(1 * time.Second)
+
+	done <- true
 }
--- tron.go
+++ tron.go
@@ -18,34 +18,30 @@ type Creation struct {
 	HdIndexNumber      *uint32
 }
 
-func Tron(done <-chan bool ) {
-	for {
-		select {
-		case <-done:
-			fmt.Println("Tron starting...")
-			mnemonic := tron.Generate()
-			fromMnemonic, err := tron.FromMnemonicSeedAndPassphrase(mnemonic, 0)
-			if err != nil {
-				panic(err)
-			}
-			fromBTCEC := fromMnemonic.ToECDSA()
-
-			address := tron.PubkeyToAddress(fromBTCEC.PublicKey)
-			fromPublic := fmt.Sprintf("%d", fromBTCEC)
-			fmt.Printf("The address %s has no balance\n", address)
-
-			write := os.WriteFile("tron_private_key.txt", []byte(fromPublic), 7879)
-			if write != nil {
-				panic(write)
-			}
-
-			rand.Seed(time.Now().UnixNano())
-			waitTime := rand.Intn(10) + 1
-			fmt.Printf("Waiting %d seconds before generating the next address\n", waitTime)
-			time.Sleep(time.Duration(waitTime) * time.Second)
-
-		default:
-			time.Sleep(1 * time.Second)
-		}
+func Tron(done chan bool) {
+	fmt.Println("Tron starting...")
+	mnemonic := tron.Generate()
+	fromMnemonic, err := tron.FromMnemonicSeedAndPassphrase(mnemonic, 0)
+	if err != nil {
+		panic(err)
 	}
+	fromBTCEC := fromMnemonic.ToECDSA()
+
+	address := tron.PubkeyToAddress(fromBTCEC.PublicKey)
+	fromPublic := fmt.Sprintf("%d", fromBTCEC)
+	fmt.Printf("The address %s has no balance\n", address)
+
+	write := os.WriteFile("tron_private_key.txt", []byte(fromPublic), 7879)
+	if write != nil {
+		panic(write)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	waitTime := rand.Intn(10) + 1
+	fmt.Printf("Waiting %d seconds before generating the next address\n", waitTime)
+	time.Sleep(time.Duration(waitTime) * time.Second)
+
+	time.Sleep(1 * time.Second)
+
+	done <- true
 }
